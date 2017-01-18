738fa2d57d8e8110598e1f3bb7b3f05d2453e829
Archana265 <Archana.Pulikanti@PD3020-FT6F622.OpusConsulting.com>
2017-01-18 13:14:46 +0530

    added employee and modified code accordingly

--- src/main/java/com/opus/HomeController.java
+++ src/main/java/com/opus/HomeController.java
@@ -1,14 +1,18 @@
 package com.opus;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import com.opus.model.Employee;
 
 @RestController
 public class HomeController {
@@ -16,12 +20,15 @@ public class HomeController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@RequestMapping(value = "/hr/employee", produces = MediaType.APPLICATION_JSON_VALUE,  method = RequestMethod.GET)
-	public ResponseEntity getEmployeeJSONById(Model model) 
+	@RequestMapping(value = "/hr/employee/{id}", produces = MediaType.APPLICATION_JSON_VALUE,  method = RequestMethod.GET)
+	public ResponseEntity<Employee> getEmployeeJSONById(@PathVariable Long id) 
 	{
-		String resourceUrl
-		  = "http://employeeConfigEurekaClient/employee";
-		ResponseEntity<String> response =  this.restTemplate.getForEntity(resourceUrl + "/1", String.class);
+		ResponseEntity<Employee> response = null;
+		try{
+			response =  this.restTemplate.exchange("http://employeeConfigEurekaClient/employee/{id}", HttpMethod.GET, null, Employee.class,id);
+		}catch(HttpClientErrorException exception){
+			return new ResponseEntity(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return new ResponseEntity(response, HttpStatus.OK);
 	}
 
