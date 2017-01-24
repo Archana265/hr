cd79826d3a769dadaa0f522f57c5d36058c0fa01
diptibansode <dipti.bansode@gmail.com>
2017-01-24 17:47:53 +0530

    hr application changes

--- src/main/java/com/opus/HomeController.java
+++ src/main/java/com/opus/HomeController.java
@@ -1,11 +1,11 @@
 package com.opus;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.appinfo.InstanceInfo.InstanceStatus;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.opus.model.Department;
 import com.opus.model.Employee;
 
@@ -23,6 +25,7 @@ public class HomeController {
 	private RestTemplate restTemplate;
 	
 	@RequestMapping(value = "/hr/employee/{id}", produces = MediaType.APPLICATION_JSON_VALUE,  method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "statusNotFound")
 	public ResponseEntity<Employee> getEmployeeJSONById(@PathVariable Long id) 
 	{
 		ResponseEntity<Employee> response = null;
@@ -39,12 +42,16 @@ public class HomeController {
 	{
 		ResponseEntity<Department> responseDept = null;
 		try{
-		//ResponseEntity<Employee> responseEmp =  this.restTemplate.exchange("http://employeeApp/employee/{id}" ,HttpMethod.GET, null, Employee.class,id);
 		responseDept =  this.restTemplate.exchange("http://DepartmentApp/department/{d_id}",HttpMethod.GET, null, Department.class,d_id);
 		}catch(HttpClientErrorException exception){
 			return new ResponseEntity<Department>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		return new ResponseEntity(responseDept, HttpStatus.OK);
 	}
-
+	
+	public ResponseEntity<Employee> statusNotFound(@PathVariable Long id)
+	{
+		return null;
+		
+	}
 }
--- src/main/java/com/opus/HrApplication.java
+++ src/main/java/com/opus/HrApplication.java
@@ -2,14 +2,21 @@ package com.opus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import com.opus.model.RibbonServiceConfiguration;
+
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard	
+@RibbonClient(name = "hrConfigEurekaClient", configuration = RibbonServiceConfiguration.class)
 public class HrApplication {
 
 	public static void main(String[] args) {
