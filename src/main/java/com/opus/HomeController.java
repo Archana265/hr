9c161deed6cf6915528c0329058b5062b663cdb9
Archana265 <Archana.Pulikanti@PD3020-FT6F622.OpusConsulting.com>
2017-01-24 16:22:27 +0530

    Added hystrix dashboard, circuit breaker and turbine

--- src/main/java/com/opus/HomeController.java
+++ src/main/java/com/opus/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.opus.model.Department;
 import com.opus.model.Employee;
 
@@ -22,18 +23,19 @@ public class HomeController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@HystrixCommand(groupKey = "notification-service", fallbackMethod = "fallbackforgetEmployeeJSONById")
 	@RequestMapping(value = "/hr/employee/{id}", produces = MediaType.APPLICATION_JSON_VALUE,  method = RequestMethod.GET)
 	public ResponseEntity<Employee> getEmployeeJSONById(@PathVariable Long id) 
 	{
 		ResponseEntity<Employee> response = null;
 		try{
-			response =  this.restTemplate.exchange("http://employeeApp/employee/{id}", HttpMethod.GET, null, Employee.class,id);
+			response =  this.restTemplate.exchange("http://employeeConfigEurekaClient/employee/{id}", HttpMethod.GET, null, Employee.class,id);
 		}catch(HttpClientErrorException exception){
 			return new ResponseEntity<Employee>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		return new ResponseEntity(response, HttpStatus.OK);
 	}
-	
+	@HystrixCommand(groupKey = "notification-service", fallbackMethod = "fallbackforgetEmployeeAndDept")
 	@RequestMapping(value = "/hr/department/{d_id}", produces = MediaType.APPLICATION_JSON_VALUE,  method = RequestMethod.GET)
 	public ResponseEntity<Department> getEmployeeAndDept(@PathVariable("d_id") String d_id) 
 	{
@@ -46,5 +48,21 @@ public class HomeController {
 		}
 		return new ResponseEntity(responseDept, HttpStatus.OK);
 	}
-
+	@RequestMapping(value = "/hr/test", produces = MediaType.APPLICATION_JSON_VALUE,  method = RequestMethod.GET)
+	@HystrixCommand(groupKey = "notification-service", fallbackMethod = "statusPageDown")
+	public ResponseEntity hytrixTest() {
+	    throw new RuntimeException("Simulating downstream system failure");
+	}
+	 public ResponseEntity statusPageDown() {
+	        return new ResponseEntity(HttpStatus.OK);
+	    }
+	public ResponseEntity<Employee> fallbackforgetEmployeeJSONById(@PathVariable Long id) 
+	{
+		return new ResponseEntity("from fall back of getEmployeeJSONById", HttpStatus.OK);
+	}
+	public ResponseEntity<Department> fallbackforgetEmployeeAndDept(@PathVariable("d_id") String d_id) 
+	{
+		return new ResponseEntity("from fall back of getEmployeeAndDept", HttpStatus.OK);
+	}
+	
 }
--- src/main/java/com/opus/HrApplication.java
+++ src/main/java/com/opus/HrApplication.java
@@ -2,14 +2,23 @@ package com.opus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrixDashboard
+@EnableHystrix
+@EnableCircuitBreaker
+@RestController
+@EnableTurbine
 public class HrApplication {
 
 	public static void main(String[] args) {
